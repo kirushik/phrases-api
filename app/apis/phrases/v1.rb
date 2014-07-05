00617d2772a0d86c938725ce152323cdc7e9151c
Kirill Pimenov <kirill@pimenov.cc>
2014-07-05 15:46:04 +0400

    Proper HTTP error code

--- app/apis/phrases/v1.rb
+++ app/apis/phrases/v1.rb
@@ -18,8 +18,12 @@ module Phrases
         requires :value, type: String
       end
       post '/' do
-        phrase = Phrase.create(value: params[:value])
-        [201, {}, phrase]
+        begin
+          phrase = Phrase.create(value: params[:value])
+          [201, {}, phrase]
+        rescue ActiveRecord::RecordNotUnique
+          error! 'This phrase is already in the database', 409
+        end
       end
     end
   end
