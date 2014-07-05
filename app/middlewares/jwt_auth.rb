7fbbbc054dd90ed4092af162e4dfb7157a079aa9
Kirill Pimenov <kirill@pimenov.cc>
2014-07-05 15:03:34 +0400

    Middleware checks tokens

--- app/middlewares/jwt_auth.rb
+++ app/middlewares/jwt_auth.rb
@@ -6,7 +6,23 @@ class JwtAuth
     @secret = options.fetch(:secret) # Will raise an exception if no secret is passed — that's OK
   end
   
+  # Slight modification of alexgenco's rack-jwt code
   def call(env)
-    @app.call(env) # TODO Here should go our JWT validation code
+    if authorization = env["HTTP_AUTHORIZATION"]
+      claim = ::JWT.decode(authorization, @secret)
+      #TODO Add token expiration check here
+      env["rack.jwt.claim"] = claim
+
+      @app.call(env)
+    else
+      unauthorized 'No JWT token provided'
+    end
+  rescue ::JWT::DecodeError
+    unauthorized 'Incorrect token'
+  end
+
+  private
+  def unauthorized reason
+    [401, {}, reason]
   end
 end
