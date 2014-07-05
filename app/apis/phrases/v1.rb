7349b96fe9f7b764dde8f52cb60409d6883d9d97
Kirill Pimenov <kirill@pimenov.cc>
2014-07-05 15:17:37 +0400

    Naive use of Phrase model

--- app/apis/phrases/v1.rb
+++ app/apis/phrases/v1.rb
@@ -1,10 +1,12 @@
+require_relative '../../models/phrase'
+
 module Phrases
   class V1 < Grape::API
     version 'v1', :using => :path
     format :json
 
     get '/' do
-      :ok
+      Phrase.count
     end
   end
 end
