93981fd0a4d1ddaee8f53652188ce14229b39908
Kirill Pimenov <kirill@pimenov.cc>
2014-07-05 15:39:39 +0400

    Phrase uniqueness in DB

--- app/models/phrase.rb
+++ app/models/phrase.rb
@@ -1,4 +1,11 @@
 class Phrase < ActiveRecord::Base
+  # Normally, one should not use DB as a validation tool and declare validations here,
+  # with ActiveRecord helpers.
+  # But in our case we've already used pg-only syntax, so we can squeeze some speed with DB-base validations
+  before_save do
+    self.canonical_value=self.value.mb_chars.downcase.to_s # This trick works with non-ascii characters
+  end
+
   def self.random
     # We will use SQL-based finder with random-offset technique
     # because the most common order-by-rand approach is slow due to full table load required
